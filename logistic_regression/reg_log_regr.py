036cd441af91a1343965e9dd6902b40b0b8439c2
Harihara Rajan Ramaswamy <harihararajanrm.cms@gmail.com>
2021-05-28 14:18:40 +0530

    implemented predict function and predicted the training accuracy

--- logistic_regression/reg_log_regr.py
+++ logistic_regression/reg_log_regr.py
@@ -80,18 +80,65 @@ def optimize(theta,x,y,lr,Lambda,num_iter):
         theta = theta - (lr*g)
     return theta,J,I
 
+def predict(x,learned_theta):
+    """
+    args
+    ----
+    x - training data set
+    y - actual output result  
+    theta - learned parameters
+
+    return
+    ------
+    pred - prediction value 
+
+    description 
+    -----------
+    note that in logistic regression, we have a 
+    threshhold value for a sigmoid function. if 
+    the sigmoid function predicts a value greater
+    than 0.5 for a particular sample, then the 
+    particular sample will be treated as a positive
+    one, (i.e) pred = 1 if not pred = 0   
+    """
+    z = x@learned_theta
+    y_p = sigmoid(z)
+    pred = np.zeros((y_p.shape))
+    for i in range(len(y_p)):
+        if y_p[i] >= 0.5:
+            pred[i] = 1
+        else:
+            pred[i] = 0
+    return pred
 if __name__ == "__main__":
     df = pd.read_csv("ex2data2.csv", header=None)
     X = np.array(df.iloc[:,0:2]) # feature matrix 
-    Y = np.array(df.iloc[:,2]) # output 
-     
+    Y = np.array(df.iloc[:,2])   # output 
+    pos = np.where(Y==1)
+    neg = np.where(Y==0) 
     x = mapFeature(X[:,0], X[:,1],6)  # (118,28)
     m,n= x.shape
     y = Y.reshape(m,1)
-    initial_theta = np.zeros((n, 1)) #   (28,1)
-    # c,g = compute_cost(x,y,initial_theta,1)
+    initial_theta = np.zeros((n, 1))  #   (28,1)
     params, J_vals, I_vals  = optimize(initial_theta,x,y,1,0.2,800)
-    plt.plot(I_vals,J_vals,c='m')
-    plt.xlabel("Iteration ")
-    plt.ylabel("Cost function $J(\Theta)$")
-    plt.show()
+    ## plot and check how the function value decreases for every iteration ##
+    # plt.plot(I_vals,J_vals,c='m')
+    # plt.xlabel("Iteration ")
+    # plt.ylabel("Cost function $J(\Theta)$")
+    
+    ## plot and check the data ##
+    # plt.scatter(X[:,0][pos],X[:,1][pos],c='g', label="y = 1")
+    # plt.scatter(X[:,0][neg],X[:,1][neg],c='r',marker='+', label = "y=0")
+    # plt.legend()
+    # plt.show()
+
+    ## accuracy of the model ##
+    """
+    one way to predict the accuracy of the trained model is by 
+    passing the training data set and the learned parameter to 
+    predict function and check how may training examples are pred.
+    correctly 
+    """
+    prediction = predict(x,params)
+    # print(sum(prediction==y))
+    print("The training accuracy is ",sum(y==prediction)/len(y)*100,"%" )
