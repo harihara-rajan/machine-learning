1550d1f16a5d8ed9eaacd84081b1765e5bf534db
Harihara Rajan Ramaswamy <harihararajanrm.cms@gmail.com>
2021-06-05 18:13:15 +0530

    implemented function to compute cost and reg cost

--- 03_muticlass_classification_NN/multiclass_neural_network.py
+++ 03_muticlass_classification_NN/multiclass_neural_network.py
@@ -10,8 +10,9 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from multiclass_classifier import *
 
-# def sigmoid(z):
-#     return (1/(1+np.exp(-z)))
+def sigmoid_gradient(z):
+    s = (1/(1+np.exp(z)))
+    return s * (1-s)
 
 def forward_pass(theta_1,theta_2,a1):
     """
@@ -38,6 +39,31 @@ def forward_pass(theta_1,theta_2,a1):
     a3 = sigmoid(z_3)
     return a3
 
+def nn_cost_function(k,x,y,theta_1,theta_2,lmd):
+
+    y_p = forward_pass(theta_1,theta_2,x) # 5000,10 
+    ## each column in y_p corresponds to prediction of 
+    ## certain number between {0to9}
+    m =  x.shape[0]
+    J = 0
+    for i in range(len(k)):
+        ##  making y to lie between {0,1}
+        y_mod = np.zeros((m,1))
+        temp = k [i]
+        pos = np.where(y==temp)
+        y_mod[pos] = 1
+        ## temp-1 is to manipulate the index, indexing in python starts from 0
+        ## k {1,2,...10} but, 
+        ## y_p [:,0] corresponds to 1
+        ## y_p [:,1] corresponds to 2 and so on 
+        J = J + sum((-y_mod * np.log(y_p[:,temp-1].reshape(m,1))) - 
+                    ((1-y_mod)*np.log(1-y_p[:,temp-1].reshape(m,1))))
+        theta_1_flatten = np.matrix.flatten(theta_1[:,1:]**2)
+        theta_2_flatten = np.matrix.flatten(theta_2[:,1:]**2)
+        reg_term = (lmd/(2*m)) * (sum(theta_1_flatten) + sum(theta_2_flatten))
+        cost_reg = J/m + reg_term
+    return J/m, cost_reg
+
 if __name__ == "__main__":
     """
     learned parameters from the NN have been given to us. 
@@ -52,7 +78,7 @@ if __name__ == "__main__":
     weights = loadmat("ex3weights.mat") # load data that contains learned params 
     data = loadmat("ex3data1.mat")
     X = data["X"]
-    m = X.shape[0] # number of training exampe
+    m  = X.shape[0] # number of training exampe
     y = data["y"]
     y = y.reshape(m,1)
     theta_1 = weights["Theta1"] # connects 1st i/p layer to hidden layer
@@ -66,3 +92,6 @@ if __name__ == "__main__":
     y_p = np.argmax(lp,axis=1)+1 # iterate row wise and throws index position of max values
     y_p = y_p.reshape(m,1)
     print("Training Set Accuracy:",sum(y_p==y)[0]/m*100,"%")
+    
+    K = [1,2,3,4,5,6,7,8,9,10]
+    J, J_R = nn_cost_function(K,x,y,theta_1,theta_2,1)
